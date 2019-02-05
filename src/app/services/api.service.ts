63cf062f0f1721d8ee302b78bdce53b6c5483636
Karthik <karthik@karthiks-air.speedport.ip>
2019-02-05 09:03:12 +0100

    commiting the code

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
-import { Observable, of, throwError} from 'rxjs';
-import { catchError, tap, map } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
 import { environment } from './../../environments/environment';
 
 import { Unit } from './../models/unit';
